ab59e5c9662ee3c269a6860e6ab5e11489efebbd
azizTarik-ui <69195041+azizTarik-ui@users.noreply.github.com>
2022-03-12 23:28:55 +0600

    admin login done

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using GrandBlueResortandSpa.Models;
 
 namespace GrandBlueResortandSpa.Controllers
 {
     public class AdminController : Controller
     {
+        GrandBlueEntities grandBlue = new GrandBlueEntities();
         // GET: Admin
         public ActionResult Index()
         {
@@ -22,6 +24,12 @@ namespace GrandBlueResortandSpa.Controllers
         public ActionResult UpdateRooms()
         {
             return View();
+        }
+
+        public ActionResult Logout()
+        {
+            Session.Clear();
+            return RedirectToAction("Index", "Home");
         }
     }
 }
