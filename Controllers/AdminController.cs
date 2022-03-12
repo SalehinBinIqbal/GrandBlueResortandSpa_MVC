e0dcbd7076b8352aa6c5c86cb3220cce580dcc5c
Salehin Bin Iqbal <salehin.rawna@gmail.com>
2022-03-12 22:59:52 +0600

    Admin Panel

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GrandBlueResortandSpa.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Members()
+        {
+            return View();
+        }
+
+        public ActionResult UpdateRooms()
+        {
+            return View();
+        }
+    }
+}
